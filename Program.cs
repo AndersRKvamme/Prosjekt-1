5bd419a77a8f58cdf556c1715ef6ba3e2cd29526
AndersRKvamme <rka092024@gmail.com>
2024-09-30 13:29:56 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Prosjekt_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string myWord = "Word";
+        Console.WriteLine(myWord);
+        string expectedName = "Anders";
+        string? name = Console.ReadLine();
+        Console.WriteLine("Hello " + name);
+        if (name == expectedName)
+        {
+        Console.WriteLine("Welcome Anders");
+        }
+        else name != expectedName
+        {
+        Console.WriteLine("Invalid user");
+        }
+    }
+}
